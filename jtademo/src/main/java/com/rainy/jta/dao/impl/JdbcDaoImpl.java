8cf20ddd4a2fae435abb8b803036f6b25cefebcd
shurun19851206 <shurun411256@163.com>
2015-04-17 08:38:28 +0800

    修改

--- jtademo/src/main/java/com/rainy/jta/dao/impl/JdbcDaoImpl.java
+++ jtademo/src/main/java/com/rainy/jta/dao/impl/JdbcDaoImpl.java
@@ -1,5 +1,11 @@
 package com.rainy.jta.dao.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class JdbcDaoImpl<T> extends BaseDaoImpl<T> {
+	
+	@Autowired
+	private JdbcTemplate jdbcBaseDao;
 	
 }
---
+++ jtademo/src/test/java/com/rainy/jta/dao/TestBaseDao.java
@@ -0,0 +1,26 @@
+package com.rainy.jta.dao;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestBaseDao extends TestCase {
+	
+	ApplicationContext applicationContext = null;
+	
+	@Before
+	public void setUp () {
+		applicationContext = new ClassPathXmlApplicationContext("/WEB-INF/dispacher-servlet.xml");
+	}
+	
+	@Test
+	public void testSave () {
+		// IBaseDao baseDao = (IBaseDao)applicationContext.containsBean("");
+		// baseDao.save("insert into a (a) value ('1')");
+		System.out.println(applicationContext.containsBean("jdbcBaseDao"));
+	}
+
+}
