acd6f45076850b909023baa732797829374f4912
shurun19851206 <rainy.shu@RAINY-SHU.corp.vipshop.com>
2015-04-23 18:24:11 +0800

    添加lucence索引

--- framework_lucence/src/main/java/com/rainy/lucence/service/IBaseService.java
+++ framework_lucence/src/main/java/com/rainy/lucence/service/IBaseService.java
@@ -2,11 +2,13 @@ package com.rainy.lucence.service;
 
 import java.io.IOException;
 
+import org.apache.lucene.queryparser.classic.ParseException;
+
 public interface IBaseService {
 	
 	/**
 	 * 添加索引
 	 */
-	public void addIndex () throws IOException;
+	public void addIndex () throws IOException, ParseException;
 
 }
